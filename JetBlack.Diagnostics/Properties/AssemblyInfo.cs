04f4f5bb4e112de308bce8c2c98d2e8ffb74fa92
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-09-13 10:15:40 +0100

    Project setup

---
+++ JetBlack.Diagnostics/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JetBlack.Diagnostics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JetBlack")]
+[assembly: AssemblyProduct("JetBlack.Diagnostics")]
+[assembly: AssemblyCopyright("Copyright © Rob Blackbourn 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7439a5f5-9a74-44f3-81ec-eb895990b824")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
