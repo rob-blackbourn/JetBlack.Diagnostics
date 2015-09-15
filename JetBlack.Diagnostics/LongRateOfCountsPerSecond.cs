135f6ffdbb2b4ce215b8243abb03652bf9abaaad
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-09-15 08:17:17 +0100

    Added rate of counts per second.

---
+++ JetBlack.Diagnostics/LongRateOfCountsPerSecond.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+
+namespace JetBlack.Diagnostics
+{
+    public class LongRateOfCountsPerSecond : ICounter
+    {
+        /// <summary>
+        /// The performance counter type.
+        /// </summary>
+        public const PerformanceCounterType CounterType = PerformanceCounterType.RateOfCountsPerSecond64;
+
+        /// <summary>
+        /// The performance counter managed by this class.
+        /// </summary>
+        public IPerformanceCounter Counter { get; private set; }
+
+                /// <summary>
+        /// Construct a single instance counter.
+        /// </summary>
+        /// <param name="factory">The factory used to create the counter.</param>
+        /// <param name="categoryName">The category of the counter.</param>
+        /// <param name="counterName">The name of the counter.</param>
+        /// <param name="readOnly">If true the counter will be read only, otherwise false.</param>
+        public LongRateOfCountsPerSecond(IPerformanceCounterFactory factory, string categoryName, string counterName, bool readOnly)
+            : this(factory.Create(categoryName, counterName, readOnly))
+        {
+        }
+
+        /// <summary>
+        /// Construct a multi instance counter.
+        /// </summary>
+        /// <param name="factory">The factory used to create the counter.</param>
+        /// <param name="categoryName">The category of the counter.</param>
+        /// <param name="counterName">The name of the counter.</param>
+        /// <param name="instanceName">The name of the instance.</param>
+        /// <param name="readOnly">If true the counter will be read only, otherwise false.</param>
+        public LongRateOfCountsPerSecond(IPerformanceCounterFactory factory, string categoryName, string counterName, string instanceName, bool readOnly)
+            : this(factory.Create(categoryName, counterName, instanceName, readOnly))
+        {
+        }
+
+        /// <summary>
+        /// Construct a read only performance counter on a remote machine.
+        /// </summary>
+        /// <param name="factory">The factory used to create the counter.</param>
+        /// <param name="categoryName">The category of the counter.</param>
+        /// <param name="counterName">The name of the counter.</param>
+        /// <param name="instanceName">The name of the instance.</param>
+        /// <param name="machineName">The machine name.</param>
+        public LongRateOfCountsPerSecond(IPerformanceCounterFactory factory, string categoryName, string counterName, string instanceName, string machineName)
+            : this(factory.Create(categoryName, counterName, instanceName, machineName))
+        {
+        }
+
+        private LongRateOfCountsPerSecond(IPerformanceCounter counter)
+        {
+            Counter = counter;
+        }
+
+        /// <summary>
+        /// The count.
+        /// </summary>
+        public long RawValue
+        {
+            get { return Counter.RawValue; }
+            set { Counter.RawValue = value; }
+        }
+
+        /// <summary>
+        /// Put the counter into a valid start state. Not this can only be done to writeable counters.
+        /// </summary>
+        public void Reset()
+        {
+            RawValue = 0;
+        }
+
+        /// <summary>
+        /// Increments the counter by one operation.
+        /// </summary>
+        /// <returns></returns>
+        public long Increment()
+        {
+            return Counter.Increment();
+        }
+
+        /// <summary>
+        /// Samples a read only performance counter.
+        /// </summary>
+        /// <returns></returns>
+        public float NextValue()
+        {
+            return Counter.NextValue();
+        }
+
+        /// <summary>
+        /// Returns the information required for a performance counter installer.
+        /// </summary>
+        /// <param name="counterName">The name of the counter.</param>
+        /// <param name="counterHelp">Helpful information about the counter.</param>
+        /// <returns>An array of data which can be used to install the counter.</returns>
+        public static CounterCreationData[] CreateCounterData(string counterName, string counterHelp)
+        {
+            return new[]
+            {
+                new CounterCreationData(counterName, counterHelp, CounterType)
+            };
+        }
+
+        /// <summary>
+        /// Release the resources held by the counter.
+        /// </summary>
+        public void Dispose()
+        {
+            Counter.Dispose();
+        }
+
+        public override string ToString()
+        {
+            return Counter.ToString();
+        }
+    }
+}
